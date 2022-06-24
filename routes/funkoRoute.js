c40af147e3d2ade986fac5a671048028b6667730
rjoelgs <rjoelgs@hotmail.com>
2022-06-23 22:30:52 -0500

    apunto del deploy

--- routes/funkoRoute.js
+++ routes/funkoRoute.js
@@ -7,7 +7,9 @@ router.get("/23", (req, res) => {
   });
 
   router.get("/funkos", async (req, res) => {
+    
     const { top } = req.query;
+    
     try {
       const funkos = top
         ? await Funko.find({ top })
@@ -24,6 +26,34 @@ router.get("/23", (req, res) => {
         };
       });
       res.json(returnFunkos);
+    } catch (error) {
+      res.status(500).json({
+        msg: "Error loading data from Database",
+        error
+      });
+    }
+  });
+
+  router.get("/funkoOne", async (req, res) => {
+    const { id } = req.query;
+    try {
+      const funko =  await Funko.findOne({ _id: id })
+        
+      const returnFunko = 
+         {
+          id: funko._id,
+          nombre: funko.nombre,
+          precio: funko.precio,
+          coleccion: funko.coleccion,
+          descripcion: funko.descripcion,
+          imagen: funko.imagen,
+          top: funko.top
+        };
+
+        console.log('id:', id)
+        console.log(returnFunko)
+
+      res.json(returnFunko);
     } catch (error) {
       res.status(500).json({
         msg: "Error loading data from Database",
